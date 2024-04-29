0d56bf205a8fbc5a0eb4e03da1ddb3a636e57fd2
Deepakydv001 <148570363+Deepakydv001@users.noreply.github.com>
2024-04-29 12:29:21 +0530

    Add files via upload

---
+++ MyFirstApp/MyFirstApp/BoardCell.swift
@@ -0,0 +1,14 @@
+//
+//  BoardCell.swift
+//  MyFirstApp
+//
+//  Created by Students on 24/04/24.
+//
+
+import UIKit
+
+class BoardCell: UICollectionViewCell
+{
+    @IBOutlet weak var image: UIImageView!
+    
+}
